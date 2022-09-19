e21d34bb93370dd1ed956281a99dae7e765c3bf7
Andrei <andreiteodorpetre@gmail.com>
2022-09-19 22:30:54 +0300

    Added size responsiveness. Added carousel transition

--- src/app/euronova/euronova.component.ts
+++ src/app/euronova/euronova.component.ts
@@ -8,12 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EuronovaComponent implements OnInit {
 
-
+  images: string [] = [];
+  animationTimeout : boolean = false;
   constructor() { }
 
   ngOnInit(): void {
+	for (let i = 1; i < 7; i++){
+		this.images.push("../../assets/carousel_images/poza"+i+".png");
+	}
+  }
 
+  public swipeCarousel(){
+	if(!this.animationTimeout){
+		let first_element = this.images.shift();
+		if(first_element == undefined){
+			console.log("unfound images");
+		}
+		this.images.push(<string>first_element);
+		this.setAnimationTimeout();
+}
 
+  }
+
+  public setAnimationTimeout(){
+	this.animationTimeout = true;
+
+	setTimeout(() => {
+		this.animationTimeout = false;
+	},1000);
   }
 
 }
